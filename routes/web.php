84049653f4ce37197185f05c64427f1485420680
fauzigalih <fauzigalihas@gmail.com>
2020-11-26 20:13:32 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'index']);
+Route::get('create', [UserController::class, 'create']);
+Route::get('edit/{user}', [UserController::class, 'edit'])->name('edit');
+Route::get('show/{user}', [UserController::class, 'show'])->name('show');
+Route::post('/', [UserController::class, 'store'])->name('store');
+Route::put('update/{user}', [UserController::class, 'update']);
+Route::delete('destroy/{user}', [UserController::class, 'destroy']);
+
+// Route::get('help', HelpController::class)->name('help');
