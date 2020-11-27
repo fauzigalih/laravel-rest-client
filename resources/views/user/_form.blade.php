fead8edf493ca3cb95e9c76781014c59fdcf5b96
fauzigalih <fauzigalihas@gmail.com>
2020-11-27 16:32:29 +0700

    Update _form.blade.php

--- resources/views/user/_form.blade.php
+++ resources/views/user/_form.blade.php
@@ -2,17 +2,18 @@
   $show = Route::currentRouteName() === 'show';
   $edit = Route::currentRouteName() === 'edit';
 @endphp
-{!! Form::open(['url' => $edit ? 'update/'.$model->id : '/', 'method' => $edit ? 'PUT' : 'POST']) !!}
+@include('layouts.alert')
+{!! Form::open(['url' => $edit ? 'update?id='.$model['id'] : '/', 'method' => $edit ? 'PUT' : 'POST']) !!}
   <div class="form-group">
     {!! Form::label('name', 'Full Name') !!}
-    {!! Form::text('name', $model->name ?? null, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), $show ? 'readonly' : 'required']) !!}
+    {!! Form::text('name', $model['name'] ?? null, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), $show ? 'readonly' : 'required']) !!}
     @error('name')
       <small class="form-text text-danger">{{ $message }}</small>
     @enderror
   </div>
   <div class="form-group">
     {!! Form::label('email', 'Email Address') !!}
-    {!! Form::email('email', $model->email ?? null, ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), $show ? 'readonly' : 'required']) !!}
+    {!! Form::email('email', $model['email'] ?? null, ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), $show ? 'readonly' : 'required']) !!}
     @error('email')
       <small class="form-text text-danger">{{ $message }}</small>
     @enderror
@@ -23,7 +24,7 @@
   </div>
   <div class="form-group">
     {!! Form::label('phone', 'Phone Number') !!}
-    {!! Form::number('phone', $model->phone ?? null, ['class' => 'form-control' . ($errors->has('phone') ? ' is-invalid' : ''), $show ? 'readonly' : 'required']) !!}
+    {!! Form::number('phone', $model['phone'] ?? null, ['class' => 'form-control' . ($errors->has('phone') ? ' is-invalid' : ''), $show ? 'readonly' : 'required']) !!}
     @error('phone')
       <small class="form-text text-danger">{{ $message }}</small>
     @enderror
